72135b07bd5a725a7f565b47cac2d984407f4351
benhardtitathiono <113670466+benhardtitathiono@users.noreply.github.com>
2023-07-26 18:16:04 +0700

    del welcome

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/game', function(){
+Route::get('/', function(){
     return view('game');
 });
